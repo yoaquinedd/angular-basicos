597f7d16803a723d017cafc7a6c98441bd88ff4d
yoaquinedd <joaquinlolvlad@gmail.com>
2023-02-01 20:15:19 -0300

    finseccion5

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+
 
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Maestro Roshi',
+    poder : 1000
+  }
+
+  constructor(){
+  }
 }
